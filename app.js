ff62173285fd60a41a58e22349d992fc9c329a21
Yulia <yulia.kovalova.741@gmail.com>
2021-02-24 20:01:38 +0200

    added burger menu mobile version

---
+++ app.js
@@ -0,0 +1,12 @@
+// burger menu mobile version
+let mobileMenu = document.querySelector(".menu-mobile-burger");
+let mainMenu = document.querySelector(".navigation");
+
+mobileMenu.addEventListener("click", function () {
+    mobileMenu.classList.toggle("active-menu")
+    if(mobileMenu.classList.contains("active-menu")) {
+        mainMenu.classList.add("active-menu")
+    } else {
+        mainMenu.classList.remove("active-menu")
+    }
+})
