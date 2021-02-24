c11aeee7bc605b67998e7626b533317204625ea1
Yulia <yulia.kovalova.741@gmail.com>
2021-02-25 01:36:40 +0200

    tried to add modal window after 50% scroll (unsuccessfully)

--- app.js
+++ app.js
@@ -4,9 +4,39 @@ let mainMenu = document.querySelector(".navigation");
 
 mobileMenu.addEventListener("click", function () {
     mobileMenu.classList.toggle("active-menu")
-    if(mobileMenu.classList.contains("active-menu")) {
+    if (mobileMenu.classList.contains("active-menu")) {
         mainMenu.classList.add("active-menu")
     } else {
         mainMenu.classList.remove("active-menu")
     }
 })
+
+
+
+// modal window 'describe' after vertical scroll 50% webpage
+let modal = document.querySelector(".modal");
+let CloseBtn = document.querySelector(".modal-close-btn");
+let bodyHeight = document.querySelector(".main").scrollHeight;
+let clientYOffset = window.pageYOffset;
+
+
+if (clientYOffset < bodyHeight / 2) {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+} else {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+}
+
+CloseBtn.addEventListener("click", function() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+})
+
+modal.addEventListener("click", function(e) {
+    if(e.target===modal) {
+        modal.classList.add("hide");
+        modal.classList.remove("show")
+    }
+})
+//не працює
