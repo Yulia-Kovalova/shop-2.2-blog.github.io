aae681d4f3ce16f1014b265794112a55fbe870b3
Yulia <yulia.kovalova.741@gmail.com>
2021-02-25 02:52:58 +0200

    added modal after 50% scroll when watched video lesson-22 (till 30min)

--- app.js
+++ app.js
@@ -14,29 +14,63 @@ mobileMenu.addEventListener("click", function () {
 
 
 // modal window 'describe' after vertical scroll 50% webpage
+// let modal = document.querySelector(".modal");
+// let closeBtn = document.querySelector(".modal-close-btn");
+// let bodyHeight = document.querySelector(".main").scrollHeight;
+// let clientYOffset = window.pageYOffset;
+
+
+// if (clientYOffset < bodyHeight / 2) {
+//     modal.classList.add("hide");
+//     modal.classList.remove("show");
+// } else {
+//     modal.classList.add("show");
+//     modal.classList.remove("hide");
+// }
+
+// closeBtn.addEventListener("click", function() {
+//     modal.classList.add("hide");
+//     modal.classList.remove("show");
+// })
+
+// modal.addEventListener("click", function(e) {
+//     if(e.target===modal) {
+//         modal.classList.add("hide");
+//         modal.classList.remove("show")
+//     }
+// })
+//не працює
+
+
+//********************************* */
+//Вирішення по відео уроцу 22
 let modal = document.querySelector(".modal");
 let closeBtn = document.querySelector(".modal-close-btn");
-let bodyHeight = document.querySelector(".main").scrollHeight;
-let clientYOffset = window.pageYOffset;
+
+function showModalByScroll() {
+    if(window.pageYOffset > document.body.scrollHeight/2) {
+        openModal()
+        window.removeEventListener("scroll",showModalByScroll)
+    }
+}
+window.addEventListener("scroll", showModalByScroll);
 
 
-if (clientYOffset < bodyHeight / 2) {
-    modal.classList.add("hide");
-    modal.classList.remove("show");
-} else {
+closeBtn.addEventListener("click", closeModal)
+
+function openModal () {
     modal.classList.add("show");
     modal.classList.remove("hide");
 }
 
-closeBtn.addEventListener("click", function() {
+function closeModal() {
     modal.classList.add("hide");
     modal.classList.remove("show");
-})
+}
 
 modal.addEventListener("click", function(e) {
+    console.log(e.target)
     if(e.target===modal) {
-        modal.classList.add("hide");
-        modal.classList.remove("show")
+        closeModal()
     }
 })
-//не працює
